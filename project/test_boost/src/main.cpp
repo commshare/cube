be5a4f929b68710fc51a8a98431ec107ca6aed19
penguin <kaishansheng@163.com>
2018-04-24 11:24:36 +0800

    test boost update

--- project/test_boost/src/main.cpp
+++ project/test_boost/src/main.cpp
@@ -10,6 +10,8 @@
 #include "test_base.h"
 #include "alarm.h"
 #include "timer_manager.h"
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp> 
 void test_strand1(int argc, char* argv[])
 {
     test_strand tt;
@@ -24,8 +26,8 @@ void test_deadline_timer1(int argc, char* argv[])
 {
     test_deadline_timer tt;
 
-    tt.test_timer_syn();
-    //tt.test_timer_asyn();
+    //tt.test_timer_syn();
+    tt.test_timer_asyn();
     //tt.test_timer_asyn_loop();
     getchar(); //换行符
     getchar();
@@ -47,11 +49,16 @@ void TestBase1(int argc, char* argv[])
     getchar();
 }
 
+void test()
+{
+    cout << "hello world" << endl;
+}
+
 void TestTimer(int argc, char* argv[])
 {
     alarm inst(9, 14, 0);
-    TimerManager::get_timer_manager().RegisterTimer(500,
-        std::bind(&alarm::on_timeout, &inst));
+    //TimerManager::get_timer_manager().Register(5000, test);
+    TimerManager::get_timer_manager().Register("11:11:00", test);
     TimerManager::get_timer_manager().start();
     getchar();
     getchar();
@@ -59,6 +66,10 @@ void TestTimer(int argc, char* argv[])
 
 int main(int argc, char* argv[])
 {
+    //simple test
+    cout << "现在时间：" 
+        << boost::posix_time::to_iso_string(boost::posix_time::microsec_clock::local_time()) << endl;
+
     int ch = '1';
     do{
         printf("the list: \n");
