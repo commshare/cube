ee47042c06fed31be81636aad1586c6295440b7a
penguin <kaishansheng@163.com>
2018-06-06 15:30:49 +0800

    update reddot

--- project/reddot/src/main.cpp
+++ project/reddot/src/main.cpp
@@ -24,8 +24,11 @@ eric     2018.4.27   1.0     Create
 #include "memory_pool.h"
 #include "file.h"
 #include "lexical_cast.h"
+#include "any.h"
 
 using namespace std;
+
+using protocal_struct = std::map<std::string, eco::Any>;
 
 class Test
 {
@@ -60,21 +63,23 @@ const char* Test::get_str() const
     return m_impl->data_.c_str();
 }
 
-void fun_default(IN eco::MessageMeta& msg)
+void fun_default(IN protocal_struct& msg)
 {
-    std::cout << __FUNCTION__  
-        <<" session_id:" << msg.get_session()
-        << " message_type:" << msg.get_type()
-        << " data:" << msg.get_data()
+    std::cout << __FUNCTION__
+        << " message_type:" << msg["type"].AnyCast<int>()
+        << " sessionid:" << msg["sessionid"].AnyCast<int>()
+        << " price:" << msg["price"].AnyCast<double>()
+        << " data:" << msg["data"].AnyCast<const char*>()
         << std::endl;
 }
 
-void fun_eco(IN eco::MessageMeta& msg)
+void fun_eco(IN protocal_struct& msg)
 {
     std::cout << __FUNCTION__
-        << " session_id:" << msg.get_session()
-        << " message_type:" << msg.get_type()
-        << " data:" << msg.get_data()
+        << " message_type:" << msg["type"].AnyCast<int>()
+        << " sessionid:" << msg["sessionid"].AnyCast<int>()
+        << " price:" << msg["price"].AnyCast<double>()
+        << " data:" << msg["data"].AnyCast<const char*>()
         << std::endl;
 }
 
@@ -93,19 +98,21 @@ int print(T value)
 int main(int argc, char **argv)
 {
     cout << "hello reddot" << endl;
-
-    cout << print(1, 2, 3, 100) << endl;
-
-    //eco::DispatchServer<uint32_t, eco::MessageMeta> dispatch;
-    //dispatch.set_default(fun_default);
-    //dispatch.set_dispatch(10001, fun_eco);
-    //dispatch.run(4);
-    //std::this_thread::sleep_for(std::chrono::seconds(3));
-    //for (int i = 0; i < 5; ++i)
-    //{
-    //    eco::MessageMeta msg(i, 10001+i, "hello world");
-    //    dispatch.post(msg);
-    //}
+    
+    eco::DispatchServer<uint32_t, protocal_struct> dispatch;
+    dispatch.set_default(fun_default);
+    dispatch.set_dispatch(10001, fun_eco);
+    dispatch.run(1);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    for (int i = 0; i < 5; ++i)
+    {
+        std::map<std::string, eco::Any> msg;
+        msg["type"] = 10000 + i;
+        msg["sessionid"] = 100;
+        msg["price"] = 99.999;
+        msg["data"] = "hello world";
+        dispatch.post(msg);
+    }
 
     getchar();
     return 0;
