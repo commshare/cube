b5d6158f6c789a29b495770ca0d4c924175a3f21
penguin <kaishansheng@163.com>
2018-06-09 09:09:42 +0800

    update liblight

--- library/liblight/clientapi.h
+++ library/liblight/clientapi.h
@@ -1,62 +1,61 @@
 #pragma once
-#include <algorithm>
 #include <list>
-#include <boost/asio.hpp>
 #include "light_ptr.h"
 #include "Buffer.h"
 
 namespace light
 {
-	class clientreceivehandle;
-	class LightService;
-	struct transaction_head;
-	struct transaction_additional;
-	class clientapi
-	{
-	public:
-		clientapi(boost::asio::ip::tcp::endpoint ed);
-		void send(const light::transaction_head& head, const char* body);
-		void set_handle(clientreceivehandle* phandle);
-		void run();
-		~clientapi();
-	private:
-		void	do_send(const light::transaction_head& head, const char* body);
-		void	do_receive(const light::transaction_head& head, const light::transaction_additional& additional, const char* body);
-		void	close(const std::string& message);
-		void	do_connect(boost::asio::ip::tcp::endpoint ed);
-		void	do_read();
-		void	do_write();
-		void	deliver( transaction_ptr trans );
-		void	pushtrans(transaction_ptr trans);
-		void	handle_read(boost::system::error_code ec, std::size_t length);
-		void	handle_write(boost::system::error_code ec, std::size_t length);
-		void	handle_connect(boost::system::error_code ec);
-		void	run_io(boost::asio::io_service* ps);
-		void	handler_write_timer( const boost::system::error_code& error );
-		void	handler_read_timer( const boost::system::error_code& error );
-		void	handler_reconnect_timer( const boost::system::error_code& error );
-		void	handler_close();
-		static const int HeartbeatTime = 10;
-		static const int ReconnectTime = 10;
-	private:
-		boost::asio::io_service io_service_;
-		light::socket_ptr socket_;
-		Buffer buffer_write_;
-		Buffer buffer_read_;
-		std::list<transaction_ptr> cache_write_;
-		work_ptr work_;
-		thread_ptr thread_;
-		clientreceivehandle* receive_handle_;
-		int write_pack_count_;
-		int write_pack_length_;
-		std::shared_ptr<boost::asio::deadline_timer> heartbeat_send_timer_; 
-		std::shared_ptr<boost::asio::deadline_timer> heartbeat_timer_; 
-		std::shared_ptr<boost::asio::deadline_timer> reconnect_timer_; 
-		transaction_ptr heartbeat_transaction_;
-		boost::asio::ip::tcp::endpoint endpoint_;
-		bool is_connected_;
-	private:
-		std::shared_ptr<LightService> service_;
-		std::string receive_buffer_;
-	};
+class clientreceivehandle;
+struct transaction_head;
+struct transaction_additional;
+class clientapi
+{
+public:
+    clientapi(boost::asio::ip::tcp::endpoint ed);
+    ~clientapi();
+    void set_handle(clientreceivehandle* phandle);
+    void run();
+    void send(const transaction_head& head, const char* body);
+
+private:
+    void run_io(boost::asio::io_service* ps);
+    void close(const std::string& message);
+    void deliver(transaction_ptr trans);
+    void pushtrans(transaction_ptr trans);
+    void do_connect(boost::asio::ip::tcp::endpoint ed);
+    void do_send(const transaction_head& head, const char* body);
+    void do_receive(const transaction_head& head, 
+        const transaction_additional& additional, const char* body);
+    void do_read();
+    void do_write();
+
+    void handle_connect(boost::system::error_code ec);
+    void handle_read(boost::system::error_code ec, std::size_t length);
+    void handle_write(boost::system::error_code ec, std::size_t length);
+    void handler_write_timer(const boost::system::error_code& error);
+    void handler_read_timer(const boost::system::error_code& error);
+    void handler_reconnect_timer(const boost::system::error_code& error);
+    void handler_close();
+
+private:
+    static const int HeartbeatTime = 10;
+    static const int ReconnectTime = 10;
+    
+    boost::asio::io_service io_service_;
+    work_ptr work_;
+    thread_ptr thread_;
+    socket_ptr socket_;
+    boost::asio::ip::tcp::endpoint endpoint_;
+    clientreceivehandle* receive_handle_;
+    Buffer buffer_read_;
+    Buffer buffer_write_;
+    std::list<transaction_ptr> cache_write_;
+    std::string receive_buffer_;
+    bool is_connected_;
+
+    std::shared_ptr<boost::asio::deadline_timer> heartbeat_send_timer_;
+    std::shared_ptr<boost::asio::deadline_timer> heartbeat_timer_;
+    std::shared_ptr<boost::asio::deadline_timer> reconnect_timer_;
+    transaction_ptr heartbeat_transaction_;
+};
 };
--- library/liblight/clientreceivehandle.h
+++ library/liblight/clientreceivehandle.h
@@ -2,12 +2,12 @@
 
 namespace light
 {
-	struct transaction_head;
-	class clientreceivehandle
-	{
-	public:
-		virtual void receive(const transaction_head& head, const char* data) = 0;
-		virtual void onfrontconnected() = 0;
-		virtual void onfrontdisconnected(int reason) = 0;
-	};
+struct transaction_head;
+class clientreceivehandle
+{
+public:
+    virtual void receive(const transaction_head& head, const char* data) = 0;
+    virtual void onfrontconnected() = 0;
+    virtual void onfrontdisconnected(int reason) = 0;
+};
 };
