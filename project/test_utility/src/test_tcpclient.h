bad9cd629fd956a0f91280dcb085dbd4c60ba775
penguin <kaishansheng@163.com>
2018-07-23 10:54:33 +0800

    update

--- project/test_utility/src/test_tcpclient.h
+++ project/test_utility/src/test_tcpclient.h
@@ -9,7 +9,7 @@
 #include "utility/clientsocket.h"
 #include "utility/address.h"
 #include "utility/state.h"
-#include "../../delta/src/protocal.h"
+
 using namespace std;
 using namespace utility;
 
@@ -45,21 +45,8 @@ public:
         m_tcpclient->connect_server(addr);
 
 
-        //req contract
-        common::MSG_HEADER msg_head;
-        size_t header_size = sizeof(common::MSG_HEADER);
-        msg_head.type = common::TYPE_MANAGERSERVER_INSTRUMENT_REQ;
-        msg_head.data_size = sizeof(common::manager_server_instrument_req);
-
-        common::manager_server_instrument_req instrument_req = { 0 };
-        instrument_req.requestid = 1;
-        strcpy(instrument_req.BrokerID, "9999");
-        strcpy(instrument_req.InvestorID, "123456");
 
         string data;
-        data.append(reinterpret_cast<char*>(&msg_head), sizeof(common::MSG_HEADER));
-        data.append(reinterpret_cast<char*>(&instrument_req), sizeof(common::manager_server_instrument_req));
-
         m_tcpclient->send_data(data);
 
         m_rec_thread = std::make_shared<std::thread>(std::bind(&test_tcpclient::run, this));
@@ -77,14 +64,6 @@ public:
             int len = m_tcpclient->receive_data(rsp);
             cout << len << endl;
 
-            common::MSG_HEADER msg_head;
-            memcpy(&msg_head, rsp.c_str(), sizeof(common::MSG_HEADER));
-            common::manager_server_instrument_rsp instrument_rsp = { 0 };
-            memcpy(&instrument_rsp, rsp.c_str() + sizeof(common::MSG_HEADER),
-                sizeof(common::manager_server_instrument_rsp));
-
-            cout << "rsp type:" << msg_head.type << endl;
-            cout << "rsp instrument:" << instrument_rsp.instrumentid << endl;
         }
     }
 private:
